b92904ef4f69b7597e326578dcffe7e1b1fdd648
wasamiam <wasamiam@gmail.com>
2018-03-02 15:46:59 -0800

    New

---
+++ Program 3/smallsh.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  // Commandline loop
+  do {
+    // Print prompt
+    printf(": ");
+    fflush();
+    // Read line
+    char* line = NULL;
+    size_t n = 2048;
+    getdelim(&line, &n, ' ', stdin)
+  } while();
+
+
+  // Exit
+}
