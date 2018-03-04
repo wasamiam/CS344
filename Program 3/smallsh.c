e75cf06da0428bfa6a50617b4931e976952a6f87
wasamiam <wasamiam@gmail.com>
2018-03-04 15:39:21 -0800

    Work

--- Program 3/smallsh.c
+++ Program 3/smallsh.c
@@ -1,19 +1,38 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(int argc, char **argv){
-
+  // Variables
+  char line[2048] = "";
+  char* token;
+  int status = 0;
   // Commandline loop
   do {
     // Print prompt
     printf(": ");
     fflush();
     // Read line
-    char* line = NULL;
-    size_t n = 2048;
-    getdelim(&line, &n, ' ', stdin)
+    int er = scanf("%s", line);
+    // Check for error and if built-in Command
+    if (er < 0) {
+      printf("%s\n", "Failed to get line.");
+    }
+    else{
+      token = strtok(line, ' '); // Get first token, which should be the command.
+      if (token == "exit" || token == "status" || token == "cd") {
+
+      }
+      // Else: command is NOT built-in
+      else{
+        
+      }
+    }
+
+
   } while();
 
 
   // Exit
+
 }
