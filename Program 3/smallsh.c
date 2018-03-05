6a767dcd49bd24a29e501cec5c0218ffbb53fb48
wasamiam <wasamiam@gmail.com>
2018-03-04 22:58:12 -0800

    Update

--- Program 3/smallsh.c
+++ Program 3/smallsh.c
@@ -11,13 +11,29 @@ int main(int argc, char **argv){
   char line[2048] = "";
   char* token;
   int status = 0;
-  int pid_array[100]; // static ints are initialized as 0
+  pid_t pid_array[100]; // static arrays are initialized as 0
   int n_pid = 0; // Number of process ids in the pid_array - used with exit command to minimize array search time.
   int err = 0; // Hold error values from functions
+  int i = 0; // hold number for loops
+
   // Commandline loop
   do {
     // Check for completed background tasks
     if (n_pid != 0) {
+      for (i = 0; i < 100; i++) {
+        if (n_pid == 0) {
+          break;
+        }
+        // Check if valid id
+        if(pid_array[i] == 0){}
+        // Wait for child
+        else{
+          if (waitpid(pid_array[i], &status, WNOHANG) == pid_array[i]) {
+            pid_array[i] = 0; // Set pid to 0
+            n_pid--;
+          }
+        }
+      }
 
     }
 
@@ -25,9 +41,8 @@ int main(int argc, char **argv){
     printf(": ");
     fflush(stdout);
     // Read line
-    err = scanf("%s", line);
     // Check for error
-    if (err < 0) {
+    if (fgets(line, 2048, stdin) == NULL) {
       printf("%s\n", "Failed to get line.");
       fflush(stdout);
     }
@@ -38,7 +53,6 @@ int main(int argc, char **argv){
       else{
         // Command is 'exit'
         if (strcmp(token, "exit") == 0) {
-          int i;
           for (i = 0; i < 100; i++) {
             // Exit once all children have been handled
             if (n_pid == 0) {
@@ -49,7 +63,7 @@ int main(int argc, char **argv){
             // Kill child process
             else{
               err = kill(pid_array[i], SIGTERM);
-              if (err < 0) {
+              if (err != 0) {
                 printf("%s\n", "Error killing child process.");
                 fflush(stdout);
               }
@@ -68,7 +82,7 @@ int main(int argc, char **argv){
           token = strtok(NULL, " "); // Should be the path to a new directory, or NULL.
           if (token == NULL) {
             err = chdir(getenv("HOME"));
-            if (err < 0) {
+            if (err != 0) {
               printf("%s\n", "Error while changing directory.");
               fflush(stdout);
             }
@@ -76,7 +90,7 @@ int main(int argc, char **argv){
           // Else: if another token is present, use as path.
           else{
             err = chdir(token);
-            if (err < 0) {
+            if (err != 0) {
               printf("%s\n", "Error while changing directory.");
               fflush(stdout);
             }
@@ -84,7 +98,47 @@ int main(int argc, char **argv){
         }
         // Else: command is NOT built-in
         else{
+          pid_t returnid = -2;
+          returnid = fork();
+          switch (returnid) {
+            case -1:
+              printf("Error - child not created");
+              break;
+            case 0:
+              char command_array[512][100]; // Holds command and args to pass to execvp()
+              command_str[511] = NULL; // Make sure last pointer is NULL
+              int ca_i = 0; // Holds current index in command_array
+              char* prev_tok; // Holds pointer to previous token - used with &
+              int arg_flag; // 0 means arguments can be entered, 1 means they cannot. Changed once redirection is found.
 
+              command_array[ca_i] = token; // First pointer is command string
+
+              // Add arguments to command_array
+              while ( (token = strtok(NULL, " ") ) != NULL) {
+                if (arg_flag == 0) {
+                  // input to command
+                  if (strcmp(token, "<")) {
+                    if ((token = strtok(NULL, " ") ) != NULL) {
+
+                    }
+                  }
+                  // output from command
+                  else if (strcmp(token, ">")) {
+                    /* code */
+                  }
+                  // Else: token is an argument
+                  else{
+                    strcat(command_str, token, 2048);
+
+                  }
+                }
+
+
+              }
+              break;
+            default:
+              break;
+          }
         }
 
       }
